357de16b1320732745c85ad7e845fc13c45e93d4
Havit_R <havitchoirul.rovik@mitrais.com>
2018-10-07 21:37:21 +0700

    Add pre-build script to fix module:metro-react-native-babel-preset issue

---
+++ android/appcenter-pre-build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# 
+# Change babel-preset
+# 
+# There is an issue on react-native-vector-icons. After it installs packages, the .babelrc file contains
+#   [module:metro-react-native-babel-preset].
+# The preset supposed to be [react-native].
+# The file located at 'node_modules/react-native-vector-icons/.babelrc'.
+# Let's monkey patching it for now, until I can find more elegant solution.
+
+echo "Write [react-native] to .babelrc file"
+
+# Remove existing file first
+rm -f ../node_modules/react-native-vector-icons/.babelrc
+
+# Write~
+echo "{ \"presets\": [\"react-native\"] }" >> ../node_modules/react-native-vector-icons/.babelrc
+
+echo "Done! .babelrc should be updated 😁"
