eb486fbc69a033a949a0f1f7aa5956e2d7340017
Havit Rovik <HavitChoirul.Rovik@mitrais.com>
2018-10-08 16:33:54 +0700

    Setup Continuous Deployment at CircleCI (#4)
    * Use CircleCI for continuous deployment to Surge
    
    * Special step for deploying artifacts
    
    * Enable deployment via circleCI's config.yml
    
    * Follow CircleCI Shell Script Best Practices
    
    * Still Permission denied, huh?
    
    * Fix SC2063: Grep uses regex, but this looks like a glob

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Build React app
+yarn build
+
+# Where branch am I?
+CURRENT_BRANCH="$(git branch | grep '\*' | cut -d ' ' -f2)"
+
+# Default domain
+DOMAIN="rnw-github-${CURRENT_BRANCH}.surge.sh"
+
+# Change domain for master branch
+if [ "$CURRENT_BRANCH" == "master" ]; then
+  DOMAIN="rnw-github.surge.sh"
+fi
+
+# Deploy
+echo "Deploying to Surge..."
+./node_modules/.bin/surge --project ./build --domain ${DOMAIN}
